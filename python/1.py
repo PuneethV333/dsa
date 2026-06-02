871de01ac237a767b6ca1ad5fa058fe70e62a4ac
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-02 22:31:47 +0530

    started-python-again

---
+++ python/1.py
@@ -0,0 +1,72 @@
+# basics
+
+
+# int
+x = 1
+
+# float
+y = 0.1
+
+# string
+z = "hello world"
+
+# print
+print(x, y, z)
+print(type(z))
+
+# operators
+
+# {+,-,*,//,%,**}
+
+print(x / y)
+
+
+# string
+
+s = "hello"
+
+# len(s)
+# s.lower()
+# s.capitalize()
+# s.upper()
+# s.split()
+# s.replace()
+# s.strip()
+
+
+s[0]  # 1st alpa.
+s[-1]  # last alpa.
+s[::-1]  # reversal of string
+
+
+# List  => can have collection of diff type of var
+
+arr = [1, 2, "str"]
+
+# arr.append(4)
+# arr.pop()
+# arr.insert()
+# arr.remove()
+# arr.sort()
+# arr.reverse()
+
+
+# for i in arr:
+#     print(i)
+
+r = range(6)
+
+for i in r:
+    print(i)
+
+
+# Tuples
+
+t = {1,2,3,"str"}
+
+# t[0] = 1  => np as tuples are immutable
+
+# Dictionaries {hash map}
+
+
+

98c750263738920fc5c4b8e54e56d7ad6fb01ead
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-08 23:04:48 +0530

    add:qns~14

---
+++ neetCode-150/14.py
@@ -0,0 +1,13 @@
+def trap(h: list[int]) -> int:
+    water = 0
+    for i in range(len(h)):
+        leftMax,rightMax = 0,0
+        for l in range(i+1):
+            leftMax = max(h[l],leftMax)
+        
+        for r in range(i,len(h)):
+            rightMax = max(h[r],rightMax)
+            
+        water += min(leftMax,rightMax)-h[i]
+        
+    return water

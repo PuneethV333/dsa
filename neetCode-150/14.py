a7521ff378981b9f5eedccd949d37fc8e05161b6
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-08 23:12:12 +0530

    add:qns~14

--- neetCode-150/14.py
+++ neetCode-150/14.py
@@ -2,7 +2,7 @@ def trap(h: list[int]) -> int:
     water = 0
     for i in range(len(h)):
         leftMax,rightMax = 0,0
-        for l in range(i+1):
+        for l in range(i+1):  # noqa: E741
             leftMax = max(h[l],leftMax)
         
         for r in range(i,len(h)):

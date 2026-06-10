a4c14a03f408ec3ca17f5b560f67dcc6fbebdcfa
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-10 20:29:11 +0530

    add:qns~21

--- neetCode-150/21.py
+++ neetCode-150/21.py
@@ -2,15 +2,15 @@ def dailyTemperatures(temperatures: list[int]) -> list[int]:
     res = [0 for _ in temperatures]
 
     for i in range(len(temperatures)):
-
         nextMax = 0
-        for j in range(i+1,len(temperatures)):
-            print(temperatures[j],temperatures[i])
+        for j in range(i + 1, len(temperatures)):
+            print(temperatures[j], temperatures[i])
             nextMax += 1
             if temperatures[j] > temperatures[i]:
-                res[i] =nextMax
+                res[i] = nextMax
                 break
-        
+
     return res
 
-print(dailyTemperatures([22,21,20]))
+
+print(dailyTemperatures([22, 21, 20]))

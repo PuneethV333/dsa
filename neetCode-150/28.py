6144a09d957143c114f960e21a97bdde896b6df1
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-12 21:00:40 +0530

    add:qns~28

--- neetCode-150/28.py
+++ neetCode-150/28.py
@@ -4,14 +4,15 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 # class Solution:
 def reverseList(head: [ListNode]) -> [ListNode]:  # noqa: F821
-        prev = None
-        crr = head
-        
-        while crr:
-            nxt = crr.next
-            crr.next = prev
-            prev = crr
-            crr = nxt
-        return prev
+    prev = None
+    crr = head
+
+    while crr:
+        nxt = crr.next
+        crr.next = prev
+        prev = crr
+        crr = nxt
+    return prev

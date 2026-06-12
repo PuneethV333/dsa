85b7ba337e7da0d8523fa5b4d07a3440ff68a321
Puneeth V <puneethvijayakumar333@gmail.com>
2026-06-12 21:00:24 +0530

    add:28

---
+++ neetCode-150/28.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# class Solution:
+def reverseList(head: [ListNode]) -> [ListNode]:  # noqa: F821
+        prev = None
+        crr = head
+        
+        while crr:
+            nxt = crr.next
+            crr.next = prev
+            prev = crr
+            crr = nxt
+        return prev
